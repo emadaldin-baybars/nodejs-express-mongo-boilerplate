ab3a66cd6738123660f9f174d32616ea497dea8f
Emadaldin Baybars <emadbeebrse@gmail.com>
2023-10-30 18:25:16 +0200

    adding services

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,9 @@
-const _User = require('../models/user.js');
 const { Response } = require('../models/response.js');
-
+const userService = require('../services/userService.js')
 
 //User
 const getUsers = async (req, res, next) => {
-    const users = await _User.find({ status: true })
+    const users = await userService.getUsers();
 
     res.status(200).send(Response("200", users, {}));
 }
